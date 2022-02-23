80f767e079c9b5da7ea12b76968b9b6825b797cc
shadman-13767 <shadman15-13767@diu.edu.bd>
2022-02-24 02:42:58 +0600

    This is calculator

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,86 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    EditText fv,sv;
+    Button add,minus,mul,div;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fv=findViewById(R.id.editTextNumber);
+        sv=findViewById(R.id.editTextNumber2);
+        tv=findViewById(R.id.textView2);
+        add=findViewById(R.id.add);
+        minus=findViewById(R.id.minus);
+        mul=findViewById(R.id.mul);
+        div=findViewById(R.id.div);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a,b,ans;
+
+                a=Integer.parseInt(fv.getText().toString());
+                b=Integer.parseInt(sv.getText().toString());
+
+                ans=a+b;
+
+                tv.setText("Ans: "+ans);
+            }
+        });
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a,b,ans;
+
+                a=Integer.parseInt(fv.getText().toString());
+                b=Integer.parseInt(sv.getText().toString());
+
+                ans=a-b;
+
+                tv.setText("Ans: "+ans);
+            }
+        });
+
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a,b,ans;
+
+                a=Integer.parseInt(fv.getText().toString());
+                b=Integer.parseInt(sv.getText().toString());
+
+                ans=a*b;
+
+                tv.setText("Ans: "+ans);
+            }
+        });
+
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a,b,ans;
+
+                a=Integer.parseInt(fv.getText().toString());
+                b=Integer.parseInt(sv.getText().toString());
+
+                ans=a/b;
+
+                tv.setText("Ans: "+ans);
+            }
+        });
+
     }
 }
